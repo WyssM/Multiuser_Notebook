10ec826e6aaa46d59c4318dbf08ba2d0cf2dd525
WyssM <michiwysss@gmail.com>
2023-12-16 14:08:38 +0100

    Added something

---
+++ backend/src/main/java/com/wiss/m223/model/Kategorien.java
@@ -0,0 +1,56 @@
+package com.wiss.m223.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Kategorien")
+public class Kategorien {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long kategorieId;
+    private String name;
+    private String beschreibung;
+
+    @ManyToOne
+    @JoinColumn(name = "erstellerId")
+    private Benutzer ersteller;
+
+    public Kategorien(Long kategorieId, String name, String beschreibung, Benutzer ersteller) {
+        this.kategorieId = kategorieId;
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.ersteller = ersteller;
+    }
+
+    public Long getKategorieId() {
+        return kategorieId;
+    }
+
+    public void setKategorieId(Long kategorieId) {
+        this.kategorieId = kategorieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public Benutzer getErsteller() {
+        return ersteller;
+    }
+
+    public void setErsteller(Benutzer ersteller) {
+        this.ersteller = ersteller;
+    }
+}
